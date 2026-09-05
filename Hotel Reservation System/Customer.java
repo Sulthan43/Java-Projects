66a47f3c26dfdb237e21d199f37cc0cef8c98d67
Sulthan43 <sulthan23032k3@gmail.com>
2026-09-05 13:41:48 +0530

    Implement Customer class for hotel reservation system

--- Hotel Reservation System/Customer.java
+++ Hotel Reservation System/Customer.java
@@ -1,1 +1,36 @@
+package customerDataHandling;
+
+class Customer{
+	
+	private static int idGen = 1;
+	
+	private final int id;
+	private String name;
+	private int roomNo;
+	
+	Customer(String name ,int roomNo){
+		this.id = idGen++;
+		this.name = name;
+		this.roomNo = roomNo;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRoomNo() {
+		return roomNo;
+	}
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+	public String toString() {
+		return "ID : "+ id + " \nName : " + name + " \nRoom No : " + roomNo;
+	}
+}
 
