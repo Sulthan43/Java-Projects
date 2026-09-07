99a6857ae9833756fe1d5cc35686c1d3e9f64f7c
Sulthan43 <sulthan23032k3@gmail.com>
2026-09-07 23:18:30 +0530

    Refactor database connection and clean up code
    Updated database connection details and fixed formatting issues.

--- Hospital Management System/ManagementClass.java
+++ Hospital Management System/ManagementClass.java
@@ -2,9 +2,9 @@ import java.sql.*;
 import java.util.*;
 
 public class HospitalManagementSystem {
-    private static final String url = "jdbc:mysql://localhost:3306/hospital";
-    private static final String username = "root";
-    private static final String password = "Admin@123";
+    private static final String url = "";
+    private static final String username = "";
+    private static final String password = "";
 
     public static void main(String[] args) {
         try{
